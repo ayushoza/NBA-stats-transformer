51defdc5cdd1ed065a9f1bee5214ecfe1dbe13e6
Janelle <janelle@MacBook.local>
2022-04-14 23:05:03 -0400

    updating preprocessing

--- backend/server/apps/ml/stats_prediction/stats_predictor.py
+++ backend/server/apps/ml/stats_prediction/stats_predictor.py
@@ -5,13 +5,11 @@ Created on Sun Apr 10 03:18:36 2022
 
 @author: janelle
 """
-import joblib
+import joblib, time
+import numpy as np
 import pandas as pd
 from nba_api.stats.static import players
-import sys
-sys.path.insert(0, 'NBA-stats-transformer/data_processing/monthly_data.py')
-
-import monthly_data as m
+from nba_api.stats.endpoints import playergamelog
 
 
 class StatsPredictor:
@@ -20,26 +18,45 @@ class StatsPredictor:
         Load preprocessing objects and Transformer object
         
         """
-        path_to_model = ""
         self.model = joblib.load("transformer_model.joblib")
         self.active_player_dict = players.get_active_players()
+        self.inputs = []
+        self.current_input = None
         
     def preprocessing(self, input_name):
         """
         Apply preprocessing to input data
 
         """
-        input_name = input_name.title()
+        input_name = input_name.lower()
         try:
             player = [player for player in self.active_player_dict if 
                       (player['full_name']).lower() == input_name][0]
             player_id = player['id']
         except IndexError:
             print("Player does not exist or is not currently active. Please try another name.")
+            return
         
-        input_stats = ...    
+        input_stats = ... 
         
         return 
+    
+        def game_to_month(player_id, season):
+            """
+            If no monthly stats can be found, turn each game
+            stats into monthly stats.
+        
+            """
+            gamelog = playergamelog.PlayerGameLog(player_id, season=season, season_type_all_star='Regular Season')
+            df = gamelog.player_game_log.get_data_frame()
+            time.sleep(0.5)
+            df_np = df.to_numpy()
+            months = [d[:3] for d in df_np[:,3]]
+            stats = df_np[:,[6,7,8,13,14,18,19,20,21,22,23,24]]
+            df_np = np.c_[stats, months]
+            new_df = pd.DataFrame(df_np, columns=('MP','FGM','FGA','FTM','FTA','REB','AST','STL','BLK','TOV','PF','PTS','Month'))
+            df = new_df.groupby(['Month'],sort=False).mean(False)
+            return df[::-1]
     
     def predict(self, input_data):
         """
