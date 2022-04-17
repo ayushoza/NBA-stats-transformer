e723a2e1e48222621ca011d0eda37b3b1dc9ed19
Janelle <janelle@MacBook.local>
2022-04-17 06:46:48 -0400

    add ml code

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -14,3 +14,25 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+# ML registry
+import inspect
+from apps.ml.registry import MLRegistry
+from apps.ml.stats_prediction.stats_predictor import StatsPredictor
+
+try:
+    registry = MLRegistry() # create ML registry
+    # Random Forest classifier
+    sp = StatsPredictor()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="stats_predictor",
+                            algorithm_object=sp,
+                            algorithm_name="transformer",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Janelle+Ayush",
+                            algorithm_description="Transformer with simple pre- and post-processing",
+                            algorithm_code=inspect.getsource(StatsPredictor))
+
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
